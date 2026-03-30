fa075e2f0895587f80cced3764fbf8bc389a9817
Gilad-Shai <157134899+Gilad-Shai@users.noreply.github.com>
2026-03-30 16:21:29 +0300

    fix: use radix-ui monorepo import in dialog.tsx

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -1,8 +1,7 @@
 "use client"
 
 import * as React from "react"
-import * as DialogPrimitive from "@radix-ui/react-dialog"
-import { X } from "lucide-react"
+import { Dialog as DialogPrimitive } from "radix-ui"import { X } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
